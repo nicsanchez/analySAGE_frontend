b2c86f4433ffbd22f497fdf8c94aca8a693ff098
David Nicolás Sanchez Sendoya <dnicolas.sanchez@udea.edu.co>
2022-12-20 18:45:28 -0500

    Implementación de carga masiva de programas y colegios

--- src/app/pages/configuration/configuration.component.ts
+++ src/app/pages/configuration/configuration.component.ts
@@ -9,11 +9,11 @@ import { ConfigurationService } from 'src/app/services/configuration.service';
 export class ConfigurationComponent {
   constructor(private configurationService: ConfigurationService) {}
 
-  public configurationData = {
+  public municipalityData = {
     card: {
       title: 'Carga masiva de municipios',
       confirmMessage: `Estas a punto de ingresar nuevos municipios al sistema`,
-      callback: this.configurationService.saveAttachments,
+      callback: this.configurationService.saveMunicipalityAttachment,
       service: this.configurationService,
       showSemester: false,
     },
@@ -21,5 +21,33 @@ export class ConfigurationComponent {
     description: `El objetivo de este cargue masivo es configurar en base de datos
     continentes, paises, departamentos y municipios de los cuales tomará como referencia
     para la carga masiva de participantes al examen de admisión.`,
+  };
+
+  public schoolData = {
+    card: {
+      title: 'Carga masiva de colegios',
+      confirmMessage: `Estas a punto de ingresar nuevos colegios al sistema`,
+      callback: this.configurationService.saveSchoolAttachment,
+      service: this.configurationService,
+      showSemester: false,
+    },
+    title: 'Parametrización de colegios',
+    description: `El objetivo de este cargue masivo es configurar en base de datos
+    los colegios almacenados en MOISES, los cuales tomará como referencia para la
+    carga masiva de participantes al examen de admision.`,
+  };
+
+  public ProgramData = {
+    card: {
+      title: 'Carga masiva de programas',
+      confirmMessage: `Estas a punto de ingresar nuevos programas al sistema`,
+      callback: this.configurationService.saveProgramAttachment,
+      service: this.configurationService,
+      showSemester: false,
+    },
+    title: 'Parametrización de programas',
+    description: `El objetivo de este cargue masivo es configurar en base de datos
+    los programas almacenados en MOISES, los cuales tomará como referencia para la
+    carga masiva de participantes al examen de admision.`,
   };
 }
--- src/app/services/configuration.service.ts
+++ src/app/services/configuration.service.ts
@@ -9,13 +9,40 @@ export class ConfigurationService {
   public headers = {};
   constructor(private http: HttpClient) {}
 
-  saveAttachments(data: any, configurationService: ConfigurationService) {
+  saveMunicipalityAttachment(
+    data: any,
+    configurationService: ConfigurationService
+  ) {
     const token = localStorage.getItem('token');
     configurationService.headers = {
       Authorization: `Bearer ${token}`,
     };
     return configurationService.http.post(
       `${environment.apiURL}/municipality/storeMunicipalities`,
+      data,
+      { headers: configurationService.headers }
+    );
+  }
+
+  saveSchoolAttachment(data: any, configurationService: ConfigurationService) {
+    const token = localStorage.getItem('token');
+    configurationService.headers = {
+      Authorization: `Bearer ${token}`,
+    };
+    return configurationService.http.post(
+      `${environment.apiURL}/school/storeSchools`,
+      data,
+      { headers: configurationService.headers }
+    );
+  }
+
+  saveProgramAttachment(data: any, configurationService: ConfigurationService) {
+    const token = localStorage.getItem('token');
+    configurationService.headers = {
+      Authorization: `Bearer ${token}`,
+    };
+    return configurationService.http.post(
+      `${environment.apiURL}/program/storePrograms`,
       data,
       { headers: configurationService.headers }
     );
